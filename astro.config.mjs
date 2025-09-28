7ac64222f8520fffa96c1e4bb9e7485b4215299d
Albor2905 <noryoku07@hotmail.com>
2025-09-28 00:35:01 -0700

    header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  integrations: [tailwind()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
