aa7fd88498a1db03e6d7d9e62821454a0c53f814
Albor2905 <noryoku07@hotmail.com>
2025-09-28 23:55:08 -0700

    hero-mediasxd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,3 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
-import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  integrations: [tailwind()],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
